e39c5864c5725c26ea9edc85d157addc1301b894
Pham Hoang Duy Long <phamduylong1742000@gmail.com>
2023-06-08 22:39:00 +0700

    fix something

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,18 +2,18 @@ const HomePage = () => {
     return (
         <>
             <br />
-            <h3>Đây là trang home</h3>
-            <p>- Sử dụng API từ trang web <strong>https://reqres.in/</strong> để tạo website</p>
-            <p>- Sử dụng thư viện React để tạo 1 website cơ bản gồm các chức năng:</p>
-            <p>+ Đăng nhập <strong>(username: long174 - password: 123)</strong></p>
+            <h3>This is home page</h3>
+            <p>- Use the API from <strong>https://reqres.in/</strong> to create website</p>
+            <p>- Use React library to create website with functions such as:</p>
+            <p>+ Log in <strong>(username: long174 - password: 123)</strong></p>
             <p>+ CRUD</p>
-            <p>+ Search user theo <strong>Email</strong></p>
-            <p>+ Sắp xếp user theo <strong>ID</strong> hoặc <strong>First name</strong></p>
-            <p>+ Phân trang</p>
-            <p>+ Import dữ liệu từ file csv</p>
-            <p>+ Export dữ liệu thành file csv</p>
+            <p>+ Search user by <strong>Email</strong></p>
+            <p>+ Sort user list by <strong>ID</strong> or <strong>First name</strong></p>
+            <p>+ Pagination</p>
+            <p>+ Import data from csv file</p>
+            <p>+ Export data to csv file</p>
             <br />
-            <p>- Thời gian hoàn thành: <strong>4 ngày</strong></p>
+            <p>- Time to complete: <strong>4 days</strong></p>
             <p>- Link github: <strong>https://github.com/long1742000/ProjectReactJS.git</strong></p>
         </>
     )
